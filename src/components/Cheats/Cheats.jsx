666460ee81f933f6a7e5e1555b5151e9b6bbd19f
Frank Rauen <home@Franks-MacBook-Air.local>
2020-06-03 20:38:59 -0600

    adds clefs image and cheats styling

--- src/components/Cheats/Cheats.jsx
+++ src/components/Cheats/Cheats.jsx
@@ -3,13 +3,17 @@ import ModalImage from 'react-modal-image';
 import styles from './Cheats.module.css';
 
 //Images
-import modes from '../Cheats/images/modes.png'
-import chords from '../Cheats/images/guitar-chords.jpg'
+import modes from '../Cheats/images/modes.png';
+import chords from '../Cheats/images/guitar-chords.jpg';
+import clefs from '../Cheats/images/clefs.jpg';
 
 class Cheats extends Component {
     render() {
         return(
             <div className={styles.modals}>
+                <header className={styles.header}>
+                    Quick Reference and Cheat Sheets
+                </header>
             <div className={styles.container}>
                 <ModalImage 
                 className ={styles.modal}
@@ -24,9 +28,18 @@ class Cheats extends Component {
                 className ={styles.modal}
                 small = {chords}
                 large = {chords}
-                alt = 'Guitar Modes'
+                alt = 'Guitar Chords'
                 />
                 <p>Basic Guitar Chord Shapes</p>
+            </div>
+            <div className={styles.container}>
+                <ModalImage 
+                className ={styles.modal}
+                small = {clefs}
+                large = {clefs}
+                alt = 'Clefs'
+                />
+                <p>Treble and Bass Clefs</p>
             </div>
             </div>
         )
