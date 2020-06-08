8a95a6824a6f00606048d7ab9ba5ebcddd9c0ab9
Frank Rauen <home@Franks-MacBook-Air.local>
2020-06-07 18:18:27 -0600

    updates sheets and artists pages

--- src/components/Sheets/Sheets.jsx
+++ src/components/Sheets/Sheets.jsx
@@ -1,23 +1,28 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import styles from './Sheets.module.css';
 import { Link } from 'react-router-dom';
+
+//images
 import EltonJohn from '../Sheets/images/EltonJohn.jpg';
+
+//links
+import eltonJohn from '../../pages/Artists/EltonJohn/EltonJohn';
+
+
 
 class Sheets extends Component {
     render() {
         return(
-            <Fragment>
-            <div className={styles.sheets}>
+            <div className='app-outer-container'>
                 <header className={styles.header}>
                     Sheet Music
                 </header>
-                <div className={styles.links}>
-                <Link to='/EltonJohn'><img src={EltonJohn} alt="Elton John"/></Link>
+                <div className={styles.links}> 
+                    <img src={EltonJohn} alt="Elton John"/>
                 </div>
             </div>
-            </Fragment>
         )
+        }
     }
-}
 
 export default Sheets;
